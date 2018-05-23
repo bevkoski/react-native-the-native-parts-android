c38da7897d22a473235b673d374ca139f3262598
Blagoja Evkoski <Blagoja.Evkoski@netcetera.com>
2018-05-23 13:04:48 +0200

    Initial commit

---
+++ android/app/src/main/java/com/example/rn/thenativeparts/NativeActivity.java
@@ -0,0 +1,14 @@
+package com.example.rn.thenativeparts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NativeActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_native);
+  }
+
+}
